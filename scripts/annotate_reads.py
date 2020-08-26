576998eadef318f3508882511c11355f926b46e9
Scott, Suzanne (H&B, North Ryde) <Suzanne.Scott@csiro.au>
2020-08-26 18:39:06 +1000

    added two seperate categories for discordant read pairs that cross more than one junction.  if the first and last jucntion side crossed is the same, a read pair is scored as 'multiple_discord', since one mate will map to host and the other to virus.  if the first and last junction side crossed is different, the read pair is scored as 'fake_discord', since reads will map either both to virus or both to host

--- scripts/annotate_reads.py
+++ scripts/annotate_reads.py
@@ -1,5 +1,19 @@
 # annotate info file with information about which reads belong to which integration
 
+# for each integration, assign reads that are informative about that integration to one of 
+# the following categories:
+# - left_chimeric: reads that are chimeric at the left boundary of the integration
+# - right_chimeric: reads that are chimeric at the right boundary of the integration
+# - left_discord: read pairs where one read maps in the host before the integration, and one read maps in the viral sequence
+# - right_discord: read pairs where one read maps in the viral sequence, and one read maps in the host sequence after the integration
+# - multiple_discord: read pairs where one read maps to virus and one to host, but span more than one integration 
+
+# there is also the following category of reads that are indirect evidence of integration
+# - fake_discord: read pairs where both reads map to either host or vector, but span more than one integration 
+#				  in other words, read pairs where one both reads map to either host or vector, but are interrupted by a sequence of the other type
+#				  for example, a pair where one read maps to the virus of one integration, and the other to the virus of the next integration
+
+
 from sys import argv
 from os import path
 from scipy.stats import norm
@@ -8,9 +22,7 @@ import argparse
 import csv
 import pysam
 
-
 import pdb
-import re
 
 def main(argv):
 	#get arguments
@@ -29,62 +41,223 @@ def main(argv):
 	
 	# iterate over integrations in info file and pull out reads crossing each one
 	# the sam/bam file has the same coordinates as the info file
-	# so just use the coordinates of the left and right junctions from this file
+	# so just use the coordinates of the left and right junctions from this files
 	with open(args.sim_info, newline='') as info_file, open(args.output, 'w', newline='') as output:
 		
 		# use csv to read and write files
 		reader = csv.DictReader(info_file, delimiter = '\t')
 		
-		writer_fieldnames = list(reader.fieldnames) + ['left_chimeric', 'right_chimeric', 'left_discord', 'right_discord', 'both_discord']
+		writer_fieldnames = list(reader.fieldnames) + ['left_chimeric', 'right_chimeric', 'left_discord', 'right_discord', 'multiple_discord', 'fake_discord']
 		
 		writer = csv.DictWriter(output, delimiter = '\t', fieldnames = writer_fieldnames)
 		writer.writeheader()
 		
-		for row in reader:
-			# pull out reads that cross the left junction
-			chr = row['chr']
-			left_start = int(row['leftStart']) 
-			left_stop = int(row['leftStop'])
-			right_start = int(row['rightStart']) 
-			right_stop = int(row['rightStop'])
-			
-			assert left_start >= 0 and right_start >= 0
-			
-			print(f"finding reads for integration {row['id']}")
+		
+		# set window size for looking for discodant pairs and 
+		# looking for multiple ints with the same discordant pair
+		window_width = window_size(args.mean_frag_len, args.sd_frag_len, args.window_frac)
+		
+		# create a buffer of integrations that all fall within this window width 
+		while True:
 
-			# find chimeric reads
-			left_chimeric = get_chimeric(chr, left_start, left_stop, samfile, args.soft_threshold)
-			right_chimeric = get_chimeric(chr, right_start, right_stop, samfile, args.soft_threshold)
+			buffer = get_ints_in_window(window_width, reader)
 			
-			row['left_chimeric'] = ";".join(left_chimeric)
-			row['right_chimeric'] = ";".join(right_chimeric)
+			# check if we've reached the end of the file
+			if len(buffer) == 0:
+				break
+		
+			# find reads crossing each integration in the buffer
+			buffer = [find_reads_crossing_ints(row, samfile, args, window_width)
+						for row in buffer]
+		
+			# check for read pairs that cross multiple junctions
+			buffer = find_multiple_discordant(buffer)
 			
-			# find discordant read pairs
-			window_width = window_size(args.mean_frag_len, args.sd_frag_len, args.window_frac)
-			left_discord = get_discordant(chr, left_start, left_stop, samfile, args.soft_threshold, window_width)
-			right_discord = get_discordant(chr, right_start, right_stop, samfile, args.soft_threshold, window_width)
-			
-			# if a read is both chimeric and discordant, chimeric takes priority
-
-			left_chimeric, left_discord = remove_chimeric_from_discord(left_chimeric, left_discord)
-			right_chimeric, right_discord = remove_chimeric_from_discord(right_chimeric, right_discord)
-					
-			# if a read crosses both the left and right boundaries, it will just be human-human and won't be picked up
-			both_discord = []
-			for read in left_discord:
-				if read in right_discord:
-					both_discord.append(read)
-					left_discord.remove(read)
-					right_discord.remove(read)
-			
-			row['left_discord'] = ";".join(left_discord)
-			row['right_discord'] = ";".join(right_discord)
-			row['both_discord'] = ";".join(both_discord)
-			
-
-			writer.writerow(row)
+			# write rows in buffer
+			for row in buffer:
+				writer.writerow(row)
 		
 	samfile.close()
+	
+def find_multiple_discordant(buffer):
+	"""
+	a read might be discordant about two or more integration sites
+	for example, if a read pair crosses the right border of one integration and the left
+	border of a close by integration, both read pairs will be mapped to the virus
+	and therefore this pair won't be detected as an integration
+	
+	deal with discordant pairs that cross multiple integrations in one of two ways:
+		1. if a pair is discordant about the right side of one integration and the left side
+		of a close by integration, then both reads are mapped to the virus and this read should
+		be removed from both.  Keep track of these in a different category ('fake_discord')
+		2. if a pair crosses right side of one integration, and both sides of a nearby integration,
+		then one read maps to virus (in the first integration), and the other maps to host (after the second)
+		and therefore this pair should be indicated to be an integration.  However, if we want to score based
+		on integration position, we need to decide which integration this read 'belongs' to.  Keep
+		these reads in a seperate category ('multiple_discord') for all the integrations that they cross,
+		to reflect the fact that the 'belong' to multiple integrations
+	"""
+	
+	# keep track of which reads we've already seen
+	seen = dict()
+	
+	# for reads that we find at multiple junctions, 
+	# keep track of which junctions so we can work out what to do with that read
+	multiples = dict()
+	
+	for i, row in enumerate(buffer):
+		# check reads found around left side
+		for read in row['left_discord'].split(';'):
+			if read in seen.keys():
+				# find out how many times we've already seen this read ID
+				n = times_already_found(read, multiples)
+				
+				# if this is the first double, add the entry from seen
+				if n == 0:
+					multiples[read] = []
+					multiples[read].append(seen[read])
+				
+				# add entry to multiples for this time we found the read
+				multiples[read].append({'int_id' : row['id'], 'side' : 'left' , 'buffer_row' : i})
+				
+			else:
+				seen[read] = {'int_id' : row['id'], 'side' : 'left', 'buffer_row' : i}
+		# check right side
+		for read in row['right_discord'].split(';'):
+			if read in seen.keys():
+				# find out how many times we've already seen this read ID
+				n = times_already_found(read, multiples)
+				
+				# if this is the first double, add the entry from seen
+				if n == 0:
+					multiples[read] = []
+					multiples[read].append(seen[read])
+				
+				# add entry to multiples for this time we found the read
+				multiples[read].append({'int_id' : row['id'], 'side' : 'right', 'buffer_row' : i})
+			else:
+				seen[read] = {'int_id' : row['id'], 'side' : 'right', 'buffer_row' : i}
+		
+		# add extra keys to dicts 
+		row['multiple_discord'] = []
+		row['fake_discord'] = []
+			
+	# deal with reads crossing multiple integrations
+	for read in multiples:
+	
+		# if the side of the first junction matches the side of the last junction
+		# this is a 'multiple_discord'
+		sides = [find['side'] for find in multiples[read]]
+		if sides[0] == sides[-1]:
+			new_type = 'multiple_discord'
+		else:
+			new_type = 'fake_discord'
+	
+		for find in multiples[read]:
+			
+			# find row and side
+			buffer_row = find['buffer_row']
+			old_type = f"{find['side']}_discord"
+			
+			# remove from list of reads of old type
+			reads = buffer[buffer_row][old_type].split(";")
+			reads.remove(read)
+			buffer[buffer_row][old_type] = ";".join(reads)
+			
+			# add to list of new type
+			buffer[buffer_row][new_type].append(f"{read}_{find['side']}")
+
+	# for each row, join lists of 'multiple_disord' and 'fake_discord'
+	for row in buffer:
+		row['multiple_discord'] = ";".join(row['multiple_discord'])
+		row['fake_discord'] = ";".join(row['fake_discord'])
+	
+	return buffer
+
+def times_already_found(read, multiples):
+	"""
+	check how many times a read has already been found when checking for discordant about multiple integration sides
+	"""	
+	if read in multiples:
+		return max(multiples[read].keys())
+	else:
+		return 0
+	
+def find_reads_crossing_ints(row, samfile, args, window_width):
+	"""
+	find reads crossing the integration site, and add them to the row
+	reads crossing the integration site can be chimeric about the left or right junction
+	or discordant about the left or right junction
+	if they're discordant about both junctions, they actually go from host sequence to host sequence
+	and	therefore aren't actually discordant
+	"""
+	
+	# get information about integration site location
+	chr = row['chr']
+	left_start = int(row['leftStart']) 
+	left_stop = int(row['leftStop'])
+	right_start = int(row['rightStart']) 
+	right_stop = int(row['rightStop'])
+	
+	assert left_start >= 0 and right_start >= 0
+	
+	print(f"finding reads for integration {row['id']}")
+
+	# find chimeric reads
+	left_chimeric = get_chimeric(chr, left_start, left_stop, samfile, args.soft_threshold)
+	right_chimeric = get_chimeric(chr, right_start, right_stop, samfile, args.soft_threshold)
+	
+	row['left_chimeric'] = ";".join(left_chimeric)
+	row['right_chimeric'] = ";".join(right_chimeric)
+	
+	# find discordant read pairs
+	
+	left_discord = get_discordant(chr, left_start, left_stop, samfile, args.soft_threshold, window_width)
+	right_discord = get_discordant(chr, right_start, right_stop, samfile, args.soft_threshold, window_width)
+	
+	# if a read is both chimeric and discordant, chimeric takes priority 
+	# (but it shouldn't be both if the clipping threshold is the same for both read types)
+	left_chimeric, left_discord = remove_chimeric_from_discord(left_chimeric, left_discord)
+	right_chimeric, right_discord = remove_chimeric_from_discord(right_chimeric, right_discord)
+			
+	row['left_discord'] = ";".join(left_discord)
+	row['right_discord'] = ";".join(right_discord)
+	
+	return row	
+			
+def get_ints_in_window(window_width, reader):
+	"""
+	get a list of integrations (dicts corresponding to one row from the int-info file)
+	that are within window_width of each other
+	"""	
+	
+	buffer = []
+		
+	# get first integration, unless we're at the end of the file
+	try:
+		first_row = next(reader)
+	except StopIteration:
+		return buffer
+	
+	first_stop = int(first_row['rightStop'])
+	last_start = int(first_row['rightStop'])
+	first_chr = first_row['chr']
+	last_chr = first_row['chr']
+	buffer.append(first_row)
+	
+	# get more integraions until we're not in the window anymore or we're at the end of the file
+	while (last_start - first_stop < window_width) and (first_chr == last_chr):
+	
+		try:
+			row = next(reader)
+			last_start = int(row['leftStart'])
+			last_chr = row['chr']
+			buffer.append(row)
+		except StopIteration:
+			break	
+		
+		
+	return buffer		
 
 def get_discordant(chr, start, stop, samfile, threshold, window_width):
 	"""
@@ -116,6 +289,7 @@ def get_discordant(chr, start, stop, samfile, threshold, window_width):
 	
 	
 	for read1, read2 in read_pair_generator(samfile, f"{chr}:{window_start}-{window_stop}"):
+	
 		# check mate is mapped
 		if read1.is_unmapped or read2.is_unmapped:
 			continue
@@ -232,7 +406,6 @@ def get_boundary(read, threshold, side = "left"):
 		# check the base is aligned
 		if (pair[1] is not None):
 			return pair[1]
-
 		
 def within(start1, stop1, start2, stop2):
 	"""
