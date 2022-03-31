38bd07566048c71a1f4f55748eed98896080143c
szsctt <suzannesctt@gmail.com>
2022-03-31 12:12:45 +1000

    Modify test script to run in docker container

--- test/runMe.sh
+++ test/runMe.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-module load singularity
-eval "$(conda shell.bash hook)"
-conda activate snakemake
-
-cd ..
-snakemake --snakefile Snakefile --configfile test/config/simulation.yml --use-singularity --cores 1
+snakemake --snakefile Snakefile --configfile test/config/simulation.yml --cores 1
