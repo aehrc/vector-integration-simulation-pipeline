a22c34a7e43606794ac019fbc60ebc0bbb793e33
Suzanne Scott <sscott@cmri.org.au>
2023-09-19 16:36:41 +1000

    added more cases

--- test/test_insert_virus.py
+++ test/test_insert_virus.py
@@ -5,7 +5,11 @@ class TestEvent:
 
     @ pytest.mark.parametrize("args, kwargs, expected", [
         ((), {}, TypeError),
-        (("", ""), {}, OSError)]
+        (("", ""), {}, OSError),
+        (("test", "test"), {}, OSError),
+        (("test/refs/test_human.fa", "test/refs/test_virus.fa"), 
+            {'fasta_extensions':"blah"}, AssertionError),
+        ],
     )
     def test_init_bad_inputs(self, args, kwargs, expected):
         with pytest.raises(expected):
